ffb682ccf75a727c62edf8986db676c77df9e2db
SetthananLA <sethanan7523@gmail.com>
2024-11-13 15:36:26 +0700

    addMethod disPlayName()

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,27 @@
 ﻿
+using System.ComponentModel;
+
 namespace ConsoleApp1
 {
     class Animal
     {
         private string name;
         public string Name { get { return name; } set { name = value; } }
+        public void disPlayName()
+        {
+            Console.WriteLine(name);
+        }
     }
 
     class Program
     {
+
+
         static void Main(string[] args)
         {
             Animal cat = new Animal();
             cat.Name = "Test";
-            Console.WriteLine(cat.Name);
+            cat.disPlayName();
         }
     }
 }
