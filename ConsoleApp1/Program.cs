47fe797c1fb6d2b67ae6c7f4537812ebcf97d56f
SetthananLA <sethanan7523@gmail.com>
2024-11-13 15:30:24 +0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ConsoleApp1
+{
+    class Animal
+    {
+        private string name;
+        public string Name { get { return name; } set { name = value; } }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            cat.Name = "Test";
+            Console.WriteLine(cat.Name);
+        }
+    }
+}
